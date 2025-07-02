cab465c4649d959dd2cb31088d1d7653dc1c485b
parrais <github@parrais.co.uk>
2025-07-02 16:38:04 +0100

    Convert to freehand drawing

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -2,11 +2,30 @@ import { useEffect, useRef } from "react";
 
 export default function Canvas() {
   const canvasRef = useRef(null);
+
   useEffect(() => {
     const canvas = canvasRef.current;
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
     const context = canvas.getContext("2d");
-    context.fillStyle = "#000000";
-    context.fillRect(50, 0, 100, 100);
+
+    const draw = (event) => {
+      const rect = canvas.getBoundingClientRect();
+      console.log(rect);
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+
+      context.beginPath();
+      context.arc(x, y, 10, 0, 2 * Math.PI);
+      context.fillStyle = "green";
+      context.fill();
+    };
+
+    canvas.addEventListener("mousemove", draw);
+
+    return () => {
+      canvas.removeEventListener("mousemove", draw);
+    };
   }, []);
 
   return <canvas ref={canvasRef}></canvas>;
