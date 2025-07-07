941aaebcb465050713ce3cac2bbbaf9ce5c67b7f
charliewturner <c.w.turner@hotmail.com>
2025-07-07 10:50:46 +0100

    Upload Konva testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Canvas from "./Components/Canvas";
+import KonvaCanvas from "./KonvaCanvas";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <header>
-        {/* <h1>Canvas</h1> */}
-      </header>
+      <header>{/* <h1>Canvas</h1> */}</header>
       <main>
-        <Canvas />
+        <KonvaCanvas />
       </main>
     </>
   );
---
+++ src/StageCopy.jsx
@@ -0,0 +1,14 @@
+// import { Stage, Layer, Rect, Circle, Line, Text } from "react-konva";
+
+// function StageCopy({ storedCanvas }) {
+//   console.log(storedCanvas);
+//   return (
+//     <Stage>
+//       <Layer>
+//         <Line {...storedCanvas.children[0].children[1]}></Line>
+//       </Layer>
+//     </Stage>
+//   );
+// }
+
+// export default StageCopy;
